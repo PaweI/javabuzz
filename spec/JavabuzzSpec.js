e5bdeffd34a3aca9945e5aedceb0380803b3974a
Pavel Redics <pavel.redics@gmail.com>
2014-11-25 21:03:29 +0000

    Prints Fizz, Buzz, JavaZZ

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,17 +2,46 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
     });
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
-      
+    });
+
+    it('divisible by 5', function(){
+      expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+    });
+
+    it('divisible by 15', function(){
+      expect(javabuzz.isDivisibleByFifteen(13)).toEqual(false);
+    });
+
+  });
+
+  describe('print', function() {
+
+    it('"Fizz" if number divisible by 3', function() {
+      expect(javabuzz.FizzBuzz(9)).toEqual("Fizz");
+    });
+
+    it('"Buzz" if number divisible by 5', function() {
+      expect(javabuzz.FizzBuzz(25)).toEqual("Buzz");
+    });
+
+    it('"JavaZZ" if number divisible by 15', function() {
+      expect(javabuzz.FizzBuzz(45)).toEqual("JavaZZ");
+    });
+
+    it('number if it is NOT divisible by 3 or 5 or 15', function() {
+      expect(javabuzz.FizzBuzz(7)).toEqual(7)
     });
 
   });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -2,4 +2,20 @@ function Javabuzz(){
   Javabuzz.prototype.isDivisibleByThree = function(number) {
     return (number % 3 === 0);
   };
+
+  Javabuzz.prototype.isDivisibleByFive = function(number) {
+    return (number % 5 === 0);
+  };
+
+  Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+    return (number % 15 === 0);
+  };
+
+  Javabuzz.prototype.FizzBuzz = function(number) {
+    if (this.isDivisibleByFifteen(number)) return "JavaZZ";
+    if (this.isDivisibleByThree(number)) return "Fizz";
+    if (this.isDivisibleByFive(number)) return "Buzz";
+    return number
+  };
 };
+
